927272a9e2c55382065d3d8c8d4b6d342827d102
Grifthh <ekrem.bulbul.52@gmail.com>
2019-03-15 18:42:57 +0300

    v0.1

---
+++ Assets/Scripts/BuildingController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingController : MonoBehaviour
+{
+    public GameObject structure;
+    public GameObject structurePreview;
+    public int rotationSpeed = 1;
+
+    private int floorLayer;
+    private GameObject tmpPreview;
+    private Vector3 offset;
+    private Vector3 hitPoint;
+    public bool isTrigger { get; set; }
+
+    private void Start()
+    {
+        floorLayer = LayerMask.GetMask("Floor");
+    }
+
+    public void Update()
+    {
+        BuildingGetKey();
+    }
+
+    private void Preview()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f, floorLayer))
+        {
+            if (tmpPreview == null)
+            {
+                hitPoint = hit.point;
+                tmpPreview = Instantiate(structurePreview, hit.point, Quaternion.identity);
+            }        }
+    }
+
+    private void PreviewMoving()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f, floorLayer))
+        {
+            offset = hit.point - hitPoint;
+            hitPoint = hit.point;
+            tmpPreview.transform.Translate(offset);
+        }
+    }
+
+    private void PreviewRotation(bool direction)
+    {
+        if (direction)
+        {
+            tmpPreview.transform.GetChild(0).gameObject.transform.Rotate(Vector3.up, 100 * rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            tmpPreview.transform.GetChild(0).gameObject.transform.Rotate(-Vector3.up, 100 * rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void BuildStructure()
+    {
+        if (!isTrigger)
+        {
+            Instantiate(structure, tmpPreview.transform.position, tmpPreview.transform.GetChild(0).gameObject.transform.rotation);
+        }
+        else
+        {
+            Debug.Log("Kurulamaz!");
+        }
+    }
+
+    private void BuildingGetKey()
+    {
+        if (tmpPreview != null)
+        {
+            PreviewMoving();
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                {
+                    BuildStructure();
+                }
+                else
+                {
+                    BuildStructure();
+                    Destroy(tmpPreview);
+                }
+            }
+            if (Input.GetKey(KeyCode.R))
+            {
+                PreviewRotation(true);
+            }
+            if (Input.GetKey(KeyCode.T))
+            {
+                PreviewRotation(false);
+            }
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Destroy(tmpPreview);
+            }
+        }
+    }
+
+    public void chooseStructure()
+    {
+        Preview();
+    }
+}
---
+++ Assets/Scripts/CollisionController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionController : MonoBehaviour
+{
+    public Material preview;
+    public Material previewCantBuild;
+
+    private GameObject buildingManeger;
+    private BuildingController buildingController;
+
+    private void Start()
+    {
+        buildingManeger = GameObject.FindGameObjectWithTag("BuildingManeger");
+        buildingController = buildingManeger.GetComponent<BuildingController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Structure"))
+        {
+            buildingController.isTrigger = true;
+            gameObject.GetComponent<MeshRenderer>().material = previewCantBuild;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Structure"))
+        {
+            buildingController.isTrigger = true;
+            gameObject.GetComponent<MeshRenderer>().material = previewCantBuild;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Structure"))
+        {
+            buildingController.isTrigger = false;
+            gameObject.GetComponent<MeshRenderer>().material = preview;
+        }
+    }
+}
