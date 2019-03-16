cc4625f27fe3710bff805a9a3315011c315793b3
Grifthhh <l1115078@std.yildiz.edu.tr>
2019-03-16 15:01:20 +0300

    v0.1.3 Camera movement and scroll added

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [System.Serializable]
+    public class CameraArea
+    {
+        public float up;
+        public float down;
+        public float left;
+        public float right;
+    }
+    public CameraArea cameraArea;
+    public float cameraSpeed = 5f;
+    public float scrollSpeed = 1f;
+
+    private void Update()
+    {
+        MoveCamera();
+    }
+
+    private void MoveCamera()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            MoveUp();
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            MoveDown();
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+
+        ZoomIn();
+        ZoomOut();
+    }
+
+    private void MoveUp()
+    {
+        Vector3 pos = transform.position;
+        pos.z += Time.deltaTime * cameraSpeed * 10;
+        transform.position = pos;
+    }
+
+    private void MoveDown()
+    {
+        Vector3 pos = transform.position;
+        pos.z -= Time.deltaTime * cameraSpeed * 10;
+        transform.position = pos;
+    }
+
+    private void MoveLeft()
+    {
+        Vector3 pos = transform.position;
+        pos.x -= Time.deltaTime * cameraSpeed * 10;
+        transform.position = pos;
+    }
+
+    private void MoveRight()
+    {
+        Vector3 pos = transform.position;
+        pos.x += Time.deltaTime * cameraSpeed * 10;
+        transform.position = pos;
+    }
+
+    private void ZoomIn()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        transform.Translate(0.0f, 0.0f,  Time.deltaTime * scroll * scrollSpeed);
+    }
+
+    private void ZoomOut()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        transform.Translate(0.0f, 0.0f, Time.deltaTime * scroll * scrollSpeed * 1000);
+    }
+}
---
+++ Assets/Scripts/Camera/LerpController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpController: MonoBehaviour
+{
+    public float cameraSmoothing = 5f;
+    public GameObject cameraOffset;
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, cameraOffset.transform.position, cameraSmoothing * Time.deltaTime);
+    }
+}
