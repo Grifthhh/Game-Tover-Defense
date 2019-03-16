d4e1dbef6a78542cde9b3f62d9a6e1293da773f8
Grifthhh <l1115078@std.yildiz.edu.tr>
2019-03-16 13:06:59 +0300

    v0.1.2 forward move added

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [System.Serializable]
     public class CameraArea
     {
         public float up;
         public float down;
         public float left;
         public float right;
+    }
+    public CameraArea cameraArea;
+    public float cameraSpeed = 1;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        moveCamera();
+    }
+
+    public void moveCamera()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 pos = transform.position;
+            pos.z += Time.deltaTime * 10;
+            transform.position = pos;
+        }
     }
 }
