21b3ad09f1b4cf2180c1897a9dbe14021de7b1df
anysworkshop <48564682+anysworkshop@users.noreply.github.com>
2019-04-23 22:55:58 +0300

    Shooter&Bullet
    PREFABS
    -Shooter
    -Bullet
    Script
    -rotControl
    -Bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private Transform target;
+    public float speed = 40f;
+
+    public void Seek(Transform target2)
+    {
+        target = target2;
+    }
+    
+    void Update()
+    {
+        if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 direction = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if(direction.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+    }
+
+    void HitTarget()
+    {
+        Destroy(gameObject);
+    }
+}
