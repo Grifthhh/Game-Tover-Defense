a90f226e3f4b2fec8ed59d3849a04460c5d2fced
Grifthhh <l1115078@std.yildiz.edu.tr>
2019-05-03 17:53:12 +0300

    start display

---
+++ Assets/Scripts/UI/FirstDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstDisplay : MonoBehaviour
+{
+    public GameOverController gameOverController;
+    
+    void Start()
+    {
+        GameController.isGamePaused = true;
+
+    }
+
+    public void Begin()
+    {
+        GameController.isGamePaused = false;
+        gameObject.SetActive(false);
+    }
+}
